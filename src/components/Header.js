a41508705261cf836f93fbd9427c71e937e5e872
João H <joaoheldermartins@hotmail.com>
2022-09-29 16:46:32 -0300

    requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { user } = this.props;
+    const value = 0;
     return (
-      <div>Header</div>
+      <header>
+        <div>
+          <h4 data-testid="email-field">{ ` Email: ${user.email}` }</h4>
+        </div>
+        <div>
+          <h4 data-testid="total-field">{`Despesa Total: ${value}`}</h4>
+          <h4 data-testid="header-currency-field">BRL</h4>
+        </div>
+      </header>
+
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+  }),
+}.isRequired;
+
+function mapStateToProps(state) {
+  const { user, wallet } = state;
+  return { user, wallet };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,4 +1,11 @@
-const initialState = {};
+const initialState = {
+  wallet: {
+    currencies: [], // array de string
+    expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+    editor: false, // valor booleano que indica de uma despesa está sendo editada
+    idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
+  },
+};
 
 function wallet(state = initialState, { type /* payload */ }) {
   switch (type) {
