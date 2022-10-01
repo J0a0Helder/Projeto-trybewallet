8586d6de5c911417ddc55a3e15649aff6cf3ce71
João H <joaoheldermartins@hotmail.com>
2022-09-30 23:48:00 -0300

    estilização pag login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import action, { fetchAPI } from '../redux/actions';
 import { EMAIL } from '../redux/reducers/user';
+import '../styles/login.css';
+import logo from './Wallet.png';
 
 class Login extends React.Component {
   state = {
@@ -40,33 +42,48 @@ class Login extends React.Component {
   render() {
     const { email, isloginButtonDisabled, senha } = this.state;
     return (
-      <form onSubmit={ this.handleSubmmit }>
-        <h1>Faça o seu Login</h1>
-        <input
-          placeholder="Insira seu Email"
-          type="text"
-          data-testid="email-input"
-          onChange={ this.handleChange }
-          value={ email }
-          name="email"
-          required
-        />
-        <input
-          placeholder="Insira sua Senha"
-          data-testid="password-input"
-          onChange={ this.handleChange }
-          value={ senha }
-          type="password"
-          name="senha"
-          required
-        />
-        <button
-          type="submit"
-          disabled={ isloginButtonDisabled }
-        >
-          Entrar
-        </button>
-      </form>
+      <main className="main-login">
+        <section className="left-login">
+          <h1>Faça o seu Login!</h1>
+          <img src={ logo } alt="wallet" className="login-img" />
+        </section>
+        <section className="right-login">
+          <form onSubmit={ this.handleSubmmit } className="card-login">
+            <h1>Login</h1>
+            <label htmlFor="email" className="text-field">
+              <h4>Email:</h4>
+              <input
+                placeholder="Insira seu Email"
+                type="text"
+                data-testid="email-input"
+                onChange={ this.handleChange }
+                value={ email }
+                name="email"
+                required
+              />
+            </label>
+            <label htmlFor="senha" className="text-field">
+              <h4>Senha:</h4>
+              <input
+                placeholder="Insira sua Senha"
+                data-testid="password-input"
+                onChange={ this.handleChange }
+                value={ senha }
+                type="password"
+                name="senha"
+                required
+              />
+            </label>
+            <button
+              type="submit"
+              disabled={ isloginButtonDisabled }
+              className="btn-login"
+            >
+              Entrar
+            </button>
+          </form>
+        </section>
+      </main>
     );
   }
 }
